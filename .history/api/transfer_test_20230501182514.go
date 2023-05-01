8c46f78eaa7b4079609a7c6e6dac6b7374121844
Ademola <kolawoleoluwasegun567@gmail.com>
2023-05-01 18:54:42 +0100

    write test for transaction list

---
+++ .history/api/transfer_test_20230501182514.go
@@ -0,0 +1,124 @@
+package api
+
+import (
+	"bytes"
+	"database/sql"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	mockdb "github.com/demola234/payzone/db/mock"
+	db "github.com/demola234/payzone/db/sqlc"
+	"github.com/demola234/payzone/utils"
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateTransfer(t *testing.T) {
+	// Create 2 new account
+	amount := int64(10)
+	account1 := generateRandomAcct(1)
+	account2 := generateRandomAcct(3)
+
+	account1.Currency = utils.USD
+	account2.Currency = utils.USD
+
+	testCases := []struct {
+		name          string
+		body          gin.H
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(recoder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			body: gin.H{
+				"from_account_id": account1.ID,
+				"to_account_id":   account2.ID,
+				"amount":          amount,
+				"currency":        utils.USD,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+
+				arg := db.TransferTxParams{
+					FromAccountID: account1.ID,
+					ToAccountID:   account2.ID,
+					Amount:        amount,
+				}
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(account1.ID)).
+					Return(account1, nil).
+					Times(1)
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account2.ID)).Times(1).Return(account2, nil)
+
+				store.EXPECT().
+					TransferTx(gomock.Any(), gomock.Eq(arg)).
+					Times(1)
+			},
+			checkResponse: func(recoder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recoder.Code)
+			},
+		},
+		{
+			name: "ToAccountNotFound",
+			body: gin.H{
+				"from_account_id": account1.ID,
+				"to_account_id":   account2.ID,
+				"amount":          amount,
+				"currency":        utils.USD,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account1.ID)).Times(1).Return(account1, nil)
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account2.ID)).Times(1).Return(db.Accounts{}, sql.ErrNoRows)
+				store.EXPECT().TransferTx(gomock.Any(), gomock.Any()).Times(0)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusNotFound, recorder.Code)
+			},
+		},
+	}
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
+
+			// Marshal body data to JSON
+			data, err := json.Marshal(tc.body)
+			require.NoError(t, err)
+
+			url := "/transfers"
+			request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, request)
+
+			tc.checkResponse(recorder)
+		})
+	}
+}
+
+func TestListTransfers(t *testing.T){
+	n := 5
+	transfers := make([]db.Transfers, n)
+	for i := 0; i < n; i++ {
+		transfers[i] = generateRandomTransfer()
+	}
+}
+
+func generateRandomAcct(id int64) db.Accounts {
+	return db.Accounts{
+		ID:       id,
+		Owner:    utils.RandomOwner(),
+		Balance:  utils.RandomMoney(),
+		Currency: utils.RandomCurrency(),
+	}
+}
---
+++ .history/api/transfer_test_20230501184934.go
@@ -0,0 +1,265 @@
+package api
+
+import (
+	"bytes"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	mockdb "github.com/demola234/payzone/db/mock"
+	db "github.com/demola234/payzone/db/sqlc"
+	"github.com/demola234/payzone/utils"
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateTransfer(t *testing.T) {
+	// Create 2 new account
+	amount := int64(10)
+	account1 := generateRandomAcct(1)
+	account2 := generateRandomAcct(3)
+
+	account1.Currency = utils.USD
+	account2.Currency = utils.USD
+
+	testCases := []struct {
+		name          string
+		body          gin.H
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(recoder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			body: gin.H{
+				"from_account_id": account1.ID,
+				"to_account_id":   account2.ID,
+				"amount":          amount,
+				"currency":        utils.USD,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+
+				arg := db.TransferTxParams{
+					FromAccountID: account1.ID,
+					ToAccountID:   account2.ID,
+					Amount:        amount,
+				}
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(account1.ID)).
+					Return(account1, nil).
+					Times(1)
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account2.ID)).Times(1).Return(account2, nil)
+
+				store.EXPECT().
+					TransferTx(gomock.Any(), gomock.Eq(arg)).
+					Times(1)
+			},
+			checkResponse: func(recoder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recoder.Code)
+			},
+		},
+		{
+			name: "ToAccountNotFound",
+			body: gin.H{
+				"from_account_id": account1.ID,
+				"to_account_id":   account2.ID,
+				"amount":          amount,
+				"currency":        utils.USD,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account1.ID)).Times(1).Return(account1, nil)
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account2.ID)).Times(1).Return(db.Accounts{}, sql.ErrNoRows)
+				store.EXPECT().TransferTx(gomock.Any(), gomock.Any()).Times(0)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusNotFound, recorder.Code)
+			},
+		},
+	}
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
+
+			// Marshal body data to JSON
+			data, err := json.Marshal(tc.body)
+			require.NoError(t, err)
+
+			url := "/transfers"
+			request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, request)
+
+			tc.checkResponse(recorder)
+		})
+	}
+}
+
+type Query struct {
+	pageID    int
+	pageSize  int
+	accountID int64
+}
+
+func TestListTransfers(t *testing.T) {
+
+	account1 := generateRandomAcct(1)
+	account2 := generateRandomAcct(2)
+	account1.Currency = utils.USD
+	account2.Currency = utils.USD
+
+	transfers := make([]db.Transfers, 0, 2)
+
+	testCases := []struct {
+		name          string
+		buildStubs    func(store *mockdb.MockStore)
+		query         Query
+		checkResponse func(recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			buildStubs: func(store *mockdb.MockStore) {
+				arg := db.ListTransfersParams{
+					Limit:  5,
+					Offset: 0,
+					FromAccountID: account1.ID,
+				}
+
+				store.EXPECT().
+					ListTransfers(gomock.Any(), gomock.Eq(arg)).
+					Times(1).
+					Return(transfers, nil)
+			},
+			query: Query{
+				pageID:    1,
+				pageSize:  5,
+				accountID: account1.ID,
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+
+			},
+		},
+	}
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
+
+			url := "/transfers"
+			request, err := http.NewRequest(http.MethodGet, url, nil)
+			require.NoError(t, err)
+
+			// Add query parameters to request URL
+			q := request.URL.Query()
+			q.Add("page_id", fmt.Sprintf("%d", tc.query.pageID))
+			q.Add("page_size", fmt.Sprintf("%d", tc.query.pageSize))
+			q.Add("account_id", fmt.Sprintf("%d", tc.query.accountID))
+			request.URL.RawQuery = q.Encode()
+			server.router.ServeHTTP(recorder, request)
+			tc.checkResponse(recorder)
+		})
+	}
+}
+
+func TestGetTransfer(t *testing.T) {
+
+	account1 := generateRandomAcct(1)
+	account2 := generateRandomAcct(2)
+	account1.Currency = utils.USD
+	account2.Currency = utils.USD
+
+	transfer := generateRandomTransfer(account1, account2)
+
+	testCases := []struct {
+		name          string
+		buildStubs    func(store *mockdb.MockStore)
+		transferID    string
+		checkResponse func(recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetTransfer(gomock.Any(), gomock.Eq(transfer.ID)).
+					Times(1).
+					Return(transfer, nil)
+			},
+			transferID: transfer.ID,
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+
+			},
+		},
+		{
+			name: "NotFound",
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetTransfer(gomock.Any(), gomock.Eq(transfer.ID)).
+					Times(1).
+					Return(db.Transfers{}, sql.ErrNoRows)
+			},
+			transferID: transfer.ID,
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusNotFound, recorder.Code)
+
+			},
+		},
+		{
+			name: "InvalidID",
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetTransfer(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			transferID: "invalid_id",
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+
+			},
+		},
+		{
+			name: "Error",
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetTransfer(gomock.Any(), gomock.Eq(transfer.ID)).
+					Times(1).
+					Return(db.Transfers{}, errors.New("some error"))
+			},
+			transferID: transfer.ID,
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+
+			},
+		},
+	}
+	
+
+func generateRandomAcct(id int64) db.Accounts {
+	return db.Accounts{
+		ID:       int64(utils.RandomInt(1, 1000)),
+		Owner:    utils.RandomOwner(),
+		Balance:  utils.RandomMoney(),
+		Currency: utils.RandomCurrency(),
+	}
+}
